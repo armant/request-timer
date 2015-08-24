06fabcfc018d241f9830ca059b2e0f8270cb5629
Arman Tokanov <armatoka@gmail.com>
2015-08-24 18:15:52 +0600

    Add table for URL durations

--- routes.js
+++ routes.js
@@ -1,10 +1,13 @@
+NUM_OF_LAST_DURATIONS = 7;
+
 module.exports = function (app) {
   app.get("/", function (req, res) {
     db = req.db;
     var collection = db.get('alldurations');
     collection.find({}, {}, function(e, durations){
       res.render('index.ejs', {
-        "durations" : durations
+        "durations" : durations,
+        "NUM_OF_LAST_DURATIONS": NUM_OF_LAST_DURATIONS
       });
     });
   });
