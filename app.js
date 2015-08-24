f41bec30c852d87af03e638c9b37966bf0253332
Arman Tokanov <armatoka@gmail.com>
2015-08-24 16:25:27 +0600

    Add rendering of the main durations database

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+// MongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/requesttimer');
+
+// Express
 var express = require('express');
 var app = express();
+
+// Make the db accessible to the router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
+
+// routes.js
 require("./routes")(app);
 
+// Server startup
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
--- routes.js
+++ routes.js
@@ -1,5 +1,11 @@
 module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.send('Hello, World!');
+  app.get("/", function (req, res) {
+    db = req.db;
+    var collection = db.get('alldurations');
+    collection.find({}, {}, function(e, durations){
+      res.render('index.ejs', {
+        "durations" : durations
+      });
     });
+  });
 };
