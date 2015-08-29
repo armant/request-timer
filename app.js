a6f63db1b21529611df68d659bf7abf539bd8799
Arman Tokanov <armatoka@gmail.com>
2015-08-29 16:22:08 +0600

    Added dynamic port assigning

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(function(req,res,next){
 });
 
 // Server startup
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
